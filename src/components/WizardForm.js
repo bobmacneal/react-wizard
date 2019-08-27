0c0d34a5f25e761095a1e6795df677aaf2315034
a602484 <bob.macneal@target.com>
2019-08-27 15:31:01 -0500

    Simplify wizard container

--- src/components/WizardForm.js
+++ src/components/WizardForm.js
@@ -9,26 +9,14 @@ const H2 = styled.h2`
   margin-left: 10px;
 `
 const WizardForm = () => {
-  const handleSubmit = event => {
-    event.preventDefault()
-    // alert(`POST to server: \n
-    //        Source: ${source} \n
-    //        Destination: ${destination} \n
-    //        Name: ${taskName} \n
-    //        Time: ${schedule}`)
-  }
-
   return (
     <WizardProvider>
       <H2>Wizard</H2>
       <StepIndicator/>
-      <form onSubmit={handleSubmit}>
-        <StepForm />
-      </form>
+      <StepForm/>
       <StepNavigator />
     </WizardProvider>
   )
 }
-
 
 export default WizardForm
