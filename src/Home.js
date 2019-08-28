1bf61834fc0fad4218344729cfa7e3f876d32860
a602484 <bob.macneal@target.com>
2019-08-28 09:55:10 -0500

    Add intro to Home page

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,15 @@ const Home = () => {
     <Fragment>
       <h1>Home</h1>
       <p>
+        <b>react-wizard</b> is a step-by-step _wizard_ application written in JavaScript with
+        <a href="https://reactjs.org/">React</a>. It demonstrates an implementation of the
+        <a href="https://reactjs.org/docs/hooks-reference.html#usecontext">useContext</a> hook to
+        share a user's input form data, i.e., application "state", across components.
+
+        All form input in this application, whether text input, checkbox, or select, is auto-generated from the
+        initial state of the context in _WizardContext.js_.
+      </p>
+      <p>
         Click <a href='/wizard'>Wizard</a> to try demo.
       </p>
     </Fragment>
