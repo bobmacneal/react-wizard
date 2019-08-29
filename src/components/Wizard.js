71718e3387fc1097fdbbd35bbec2839b02c6c19a
a602484 <bob.macneal@target.com>
2019-08-29 09:03:52 -0500

    Rename

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -6,9 +6,9 @@ import styled from 'styled-components'
 import {WizardProvider} from "./WizardContext";
 
 const H2 = styled.h2`
-  margin-left: 10px;
+  margin-left: 12px;
 `
-const WizardForm = () => {
+const Wizard = () => {
   return (
     <WizardProvider>
       <H2>Wizard</H2>
@@ -19,4 +19,4 @@ const WizardForm = () => {
   )
 }
 
-export default WizardForm
+export default Wizard
